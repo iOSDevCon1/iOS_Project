bae8ce2e3f8743fd4adb902921d1cb88b946ac39
hamt3ch <hmiles23@ufl.edu>
2015-09-30 19:29:37 -0400

    Added Starter Project

---
+++ PodDrop/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PodDrop
+//
+//  Created by Hugh A. Miles on 9/30/15.
+//  Copyright (c) 2015 HAM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
