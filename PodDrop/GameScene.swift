c6b1c3f4faf7c6f26c584113bd3368f1f321eed5
Ari <ari@macbook-pro.local>
2015-10-05 11:02:01 -0400

    Changed the background anchor to 0,1 (top left) and the positioning to 0,0 because its just a background and will likely not move. This is cleaner to interpret than having the anchor at the center and the position dynamically set to the center

--- PodDrop/GameScene.swift
+++ PodDrop/GameScene.swift
@@ -22,8 +22,8 @@ class GameScene: SKScene {
         anchorPoint = CGPoint(x: 0, y: 1.0)
         
         let background = SKSpriteNode(imageNamed: "milkyWay")
-        background.position = CGPoint(x: size.width/2, y: -(size.height/2))
-        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
         background.alpha = 0.1
         addChild(background)
     }
