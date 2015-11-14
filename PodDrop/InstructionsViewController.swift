5ca99bf0862109f2e86ab43a92f7a6d2ad9d5d0e
Ari <ari@macbook-pro.local>
2015-11-14 13:26:17 -0500

    Main menu has been added, respective view controllers have been added (Leaderboard, signin, instructions)

---
+++ PodDrop/InstructionsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InstructionsViewController.swift
+//  PodDrop
+//
+//  Created by Ari on 11/14/15.
+//  Copyright © 2015 iOSDevCon1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+class InstructionsViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
---
+++ PodDrop/MainMenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainMenuViewController.swift
+//  PodDrop
+//
+//  Created by Ari on 11/14/15.
+//  Copyright © 2015 iOSDevCon1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+class MainMenuViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
