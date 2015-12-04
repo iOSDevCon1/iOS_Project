2a8c4cea0a52bbfdd3247de85bf974c960e42ce5
Ari <arif93@ufl.edu>
2015-12-04 01:01:53 -0500

    slow down item !!

--- PodDrop/ItemFactory.swift
+++ PodDrop/ItemFactory.swift
@@ -9,7 +9,7 @@ class ItemFactory {
 
     static func getRandom(size: CGSize, position:CGPoint, scrollSpeed:CGFloat, scroller:ScrollHandler) -> Item {
 
-        let randomInt = arc4random_uniform(6)
+        let randomInt = arc4random_uniform(7)
         //print(randomInt);
 
         switch randomInt{
@@ -23,6 +23,8 @@ class ItemFactory {
             return Invincibility(size: size, position:position, scrollSpeed:scrollSpeed, scroller:scroller);
         case 5:
             return Blurry(size: size, position:position, scrollSpeed:scrollSpeed, scroller:scroller);
+        case 6:
+            return SlowDown(size: size, position: position, scrollSpeed: scrollSpeed, scroller: scroller)
         default:
             return Invincibility(size: size, position:position, scrollSpeed:scrollSpeed, scroller:scroller);
         }
---
+++ PodDrop/SlowDown.swift
@@ -0,0 +1,42 @@
+//
+//  SlowDown.swift
+//  PodDrop
+//
+//  Created by Ari on 12/4/15.
+//  Copyright © 2015 iOSDevCon1. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SlowDown: Item {
+    
+    init(size: CGSize, position:CGPoint, scrollSpeed:CGFloat, scroller:ScrollHandler) {
+        //let myTexture = SKTexture(imageNamed: "dragon_ball")
+        super.init(texture: nil, color: UIColor.redColor(), size: size, position: position, scrollSpeed: scrollSpeed, scroller: scroller);
+        
+        self.name = "Slow Down"
+    }
+    
+    override func touchedBy(pod: Pod) {
+        super.touchedBy(pod)
+        self.use();
+    }
+    
+    override func use() {
+        super.use();
+        myScroller.SCROLL_SPEED = myScroller.SCROLL_SPEED * 0.80
+        myScroller.endAllItems()
+        performSelector("end", withObject: nil, afterDelay: 10)
+        
+    }
+    
+    override func end(){
+        myScroller.gameScene.podInvincible = false
+        super.end();
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
